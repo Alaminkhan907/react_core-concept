6b14f7141d806a62290c5478b6c9e07593203927
mdkhaz <mdkhaz@ttu.ee>
2021-02-24 00:59:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ function App() {
   const products = [
     {name:'photoshop', price:99},
     {name:'Illustator',price:19.99},
-    {name:'pdf reader',price:9.99}
+    {name:'pdf reader',price:9.99},
+    {name:'after effect',price:109.99}
     ]
   return (
     <div className="App">
       <header className="App-header">
-        <ProductAll product={products[0]}></ProductAll>
+        <ul>
+          {
+            products.map(pd=><li>{pd.name}</li>)
+          }
+        </ul>
+        {
+        products.map(pd=> <Product product={pd}></Product>) 
+        }
       </header>
     </div>
   );
 }
 
-function ProductAll(props){
-  console.log(props);
+function Product(props){
+  //console.log(props);
   const productStyle={
     border: '1px solid gray',
     borderRadius: '5px',
@@ -25,10 +33,12 @@ function ProductAll(props){
     width:'400px',
     float:'left'
   }
+  const {name , price}=props.product;
+  //console.log(name,price);
   return(
     <div style={productStyle}>
-      <h3>Name:{props.product.name}</h3>
-      <h5>price:{props.product.price}</h5>
+      <h3>Name:{name}</h3>
+      <h5>price:{price}</h5>
       <button>Buy now</button>
     </div>
   )
