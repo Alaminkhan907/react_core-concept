67f0217c114d267a9d476a6518bdbe977125bd7d
mdkhaz <mdkhaz@ttu.ee>
2021-02-24 00:27:29 +0200

    learn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name:'photoshop', price:99},
+    {name:'Illustator',price:19.99},
+    {name:'pdf reader',price:9.99}
+    ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          git setup done
-        </p>
-        
+        <ProductAll product={products[0]}></ProductAll>
       </header>
     </div>
   );
+}
+
+function ProductAll(props){
+  console.log(props);
+  const productStyle={
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor:'green',
+    height:'200px',
+    width:'400px',
+    float:'left'
+  }
+  return(
+    <div style={productStyle}>
+      <h3>Name:{props.product.name}</h3>
+      <h5>price:{props.product.price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
 }
 
 export default App;
